bc7d90c943db63119f0767b5926749639d897fb3
rahul0021-ts <supekar0021@gmail.com>
2025-05-08 10:22:47 +0530

    Create Example.cpp

---
+++ Example.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main(){
+  int a;
+  int b;
+  int c;
+
+  cout<<"Enter value of a:>"<<endl;
+  cin>>a;
+  cout<<"Enter value of b:>"<<endl;
+  cin>>b;
+
+  c=a+b;
+  cout<<"The Addition of a and b is : "<<c;
+  
+return 0;
+}
